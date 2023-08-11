0f2ee95f9efe4ec7e1d61c9e2e762100652a9231
youn410 <youn410@gmail.com>
2023-08-11 17:37:27 +0900

    feat: add astro file

---
+++ astro/src/interfaces/restaurant.ts
@@ -0,0 +1,10 @@
+export default interface Restaurant {
+  id: number;
+  attributes: {
+    name: string;
+    description: string;
+    createdAt: string;
+    updatedAt: string;
+    publishedAt: string;
+  }
+}
